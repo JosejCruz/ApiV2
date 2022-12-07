f9340395029dc1c2f71bf8ab8a57cee033b555ca
JosejCruz <jcruz@medicaresoftsrl.com>
2022-12-07 16:40:18 -0400

    commit v1.08
    funcion subir archivo funciona, pero hace el envio 2 veces. la primera con error.

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,127 +1,154 @@
-const { Router } = require('express')
+const { Router } = require("express");
 const router = Router();
+
+//para envio de archivo
+const FormData = require("form-data");
+const axios = require("axios");
 
 //Para manipulacion de archivos
 const os = require("node:os");
 const { exec } = require("child_process");
 const { copyFile } = require("fs/promises");
-const  fs  = require("fs");
+const fs = require("fs");
 const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
-const Plantillas = os.homedir() + '/Estudios/Templates/';
-const Temporal = os.homedir() + '/Estudios/Tmp/';
-const Final = os.homedir() + '/Estudios/Final/';
+const Plantillas = os.homedir() + "/Estudios/Templates/";
+const Temporal = os.homedir() + "/Estudios/Tmp/";
+const Final = os.homedir() + "/Estudios/Final/";
 
 //RUTAS
-router.get('/', (req, res) =>{
-    res.json({
-        message: 'ok'
-    })
-})
+router.get("/", (req, res) => {
+  res.json({
+    message: "ok",
+  });
+});
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    res.json({
-        message: 'received'
-    })
-})
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({
+    message: "received",
+  });
+});
 
-router.get('/check', (req, res) =>{
-    res.json({
-        status: 'true'
-    })
-})
+router.get("/check", (req, res) => {
+  res.json({
+    status: "true",
+  });
+});
 
 // rutas y funciones para manipulacion de archivos
-router.get('/api/url/:uid', (req, res) =>{
-    var data = req.params.uid
-    console.log(data)
-    res.json({
-        message: 'UID received'
-    })
-})
+router.get("/api/url/:uid", (req, res) => {
+  var data = req.params.uid;
+  console.log(data);
+  res.json({
+    message: "UID received",
+  });
+});
 
-router.post('/api/estudio/', (req, res)=>{
-    console.log(req.body);
-    const UID = req.body.UID
-    const Estudio = req.body.Estudio
-    //Copiar(UID, Estudio)
-    const content = fs.readFileSync((Plantillas + Estudio + '.docx'),"binary");
-    
-    const zip = new PizZip(content);
-    
-    const doc = new Docxtemplater(zip, {
-        paragraphLoop: true,
-        linebreaks: true,
-    });
-    
-    doc.render({
-        Direccion: "Direccion de prueba",
-        Fecha: "05-12-2022",
-        Hora: "14:30 pm",
-    });
-    const buf = doc.getZip().generate({
-        type: "nodebuffer",
-        compression: "DEFLATE",
-    });
-    fs.writeFileSync(Temporal + UID + '.docx', buf);
-    ////////////
+router.post("/api/estudio/", (req, res) => {
+  console.log(req.body);
+  const UID = req.body.UID;
+  const Estudio = req.body.Estudio;
+  //Copiar(UID, Estudio)
+  const content = fs.readFileSync(Plantillas + Estudio + ".docx", "binary");
 
-    exec ("open " + Temporal + UID + '.docx', (error, stdout, stderr) => {
-        if (error) {
-        res.json({
-            status: "Not Found",
-        });
-        }
-        if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-        }
-        res.json({
-        status: "Open",
-        });
-    })
-    //verCambios(Temporal + UID + '.txt')
-    //verCambios2(Temporal)
-    verCambios2(Final, UID + '.docx')
-})
+  const zip = new PizZip(content);
 
-async function Copiar(UID, Estudio){
-    try {
-        await copyFile(Plantillas + Estudio + '.docx', Temporal + UID + '.docx')
-        console.log('Archivo copiado')
-    } catch (error) {
-        console.log('error')
-        res.json({
-            message: 'error'
-        })
+  const doc = new Docxtemplater(zip, {
+    paragraphLoop: true,
+    linebreaks: true,
+  });
+
+  doc.render({
+    Direccion: "Direccion de prueba",
+    Fecha: "05-12-2022",
+    Hora: "14:30 pm",
+  });
+  const buf = doc.getZip().generate({
+    type: "nodebuffer",
+    compression: "DEFLATE",
+  });
+  fs.writeFileSync(Temporal + UID + ".docx", buf);
+  ////////////
+
+  exec("open " + Temporal + UID + ".docx", (error, stdout, stderr) => {
+    if (error) {
+      res.json({
+        status: "Not Found",
+      });
     }
-}
-function verCambios(UID){
-    fs.watch(UID, (event, filename)=>{
-        console.log(`${filename} archivos modificado`)
-    })
-}
-function verCambios2(UID, nombre){
-    fs.watchFile(UID, () => {
-        console.log(`El contenido de la carpeta ${UID} ha cambiado`);
-        if (SubirArchivo( UID + nombre)) {
-            eliminar(UID + nombre)
-            fs.unwatchFile(UID);
-            eliminar(Temporal + nombre)
-        } else {
-            console.log('error al eliminar');
-        }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    res.json({
+      status: "Open",
     });
+  });
+  //verCambios(Temporal + UID + '.txt')
+  //verCambios2(Temporal)
+  verCambios2(Final, UID + ".docx");
+});
+
+async function Copiar(UID, Estudio) {
+  try {
+    await copyFile(Plantillas + Estudio + ".docx", Temporal + UID + ".docx");
+    console.log("Archivo copiado");
+  } catch (error) {
+    console.log("error");
+    res.json({
+      message: "error",
+    });
+  }
 }
-function eliminar(UID){
-    fs.unlinkSync(UID)
+function verCambios(UID) {
+  fs.watch(UID, (event, filename) => {
+    console.log(`${filename} archivos modificado`);
+  });
+}
+function verCambios2(UID, nombre) {
+  fs.watchFile(UID, () => {
+    console.log(`El contenido de la carpeta ${UID} ha cambiado`);
+    const form = new FormData();
+  form.append("file", fs.createReadStream(UID + nombre));
+  axios
+  .post("https://file.io", form)
+  .then((res) =>{
+      if (res.data.success === true) {
+          eliminar(UID + nombre);
+          fs.unwatchFile(UID);
+          eliminar(Temporal + nombre);
+          console.log("Archivo enviado");
+        } else {
+          console.log("error al eliminar");
+          console.log('error al subir archivo')
+        }
+        console.log(res.data)
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+  });
+}
+function eliminar(UID) {
+  fs.unlinkSync(UID);
 }
 
-function SubirArchivo(Archivo){
-    //Metodo para subir archivo a servidor
-    console.log('Archivo enviado')
-    return true;
+//Metodo para subir archivo a servidor
+function SubirArchivo(Archivo) {
+  const form = new FormData();
+  form.append("file", fs.createReadStream(Archivo));
+  console.log("Archivo enviado");
+  axios
+    .post("https://file.io", form)
+    .then((res) =>{
+        console.log(res.data)
+        return true;
+    })
+    .catch((err) => {
+        console.log(err)
+        return false;
+    });
 }
 
 module.exports = router;
--- src/templates/dir.js
+++ src/templates/dir.js
@@ -1,76 +1,95 @@
 const os = require("node:os");
 const { exec } = require("child_process");
+const  fs  = require("fs");
 
-const sistema = os.platform()
-const dirHome = os.homedir()
+const sistema = os.platform();
+const dirHome = os.homedir();
 
 function CarpetasMacLinux() {
-    const instrucciones = 'cd ' + dirHome + ' && mkdir Estudios && mkdir Estudios/Templates && mkdir Estudios/Tmp && mkdir Estudios/Final';
-    const copiarplantillas = 'cp ./src/templates/docs/* ' + dirHome + '/Estudios/Templates'
-    exec(instrucciones , (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log('carpetas creadas');
-    });
-    exec(copiarplantillas , (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log('plantillas copiadas');
-    });
+  const instrucciones =
+    "cd " +
+    dirHome +
+    " && mkdir Estudios && mkdir Estudios/Templates && mkdir Estudios/Tmp && mkdir Estudios/Final";
+  const copiarplantillas =
+    "cp ./src/templates/docs/* " + dirHome + "/Estudios/Templates";
+  exec(instrucciones, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log("carpetas creadas");
+  });
+  exec(copiarplantillas, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log("plantillas copiadas");
+  });
 }
 function CarpetasWindows() {
-    const instrucciones = 'cd ' + dirHome + ' && mkdir Estudios && mkdir Estudios/Templates && mkdir Estudios/Tmp && mkdir Estudios/Final'
-    const copiarplantillas = 'copy ./src/templates/docs/* ' + dirHome + '/Estudios/Templates'
-    exec(instrucciones , (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log('carpetas creadas');
-    });
-    exec(copiarplantillas , (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log('plantillas copiadas');
-    });
+  const instrucciones =
+    "cd " +
+    dirHome +
+    " && mkdir Estudios && mkdir Estudios/Templates && mkdir Estudios/Tmp && mkdir Estudios/Final";
+  const copiarplantillas =
+    "copy ./src/templates/docs/* " + dirHome + "/Estudios/Templates";
+  exec(instrucciones, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log("carpetas creadas");
+  });
+  exec(copiarplantillas, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log("plantillas copiadas");
+  });
 }
 
-const Carpetas = ()=>{
-    switch (sistema) {
-        case 'darwin':
-            console.log(sistema)
-            CarpetasMacLinux()
-            break;
-        case 'win32':
-            console.log(sistema)
-            CarpetasWindows()
-            break;
-        default:
-            console.log('sin datos')
-            break;
-    }
-}
+const Carpetas = () => {
+  switch (sistema) {
+    case "darwin":
+      if (!fs.existsSync(dirHome + "/Estudios/")) {
+          console.log("Creando carpetas");
+          console.log(sistema);
+          CarpetasMacLinux();
+      } else {
+        console.log("Las carpetas existen");
+      }
+      break;
+    case "win32":
+        if (!fs.existsSync(dirHome + "/Estudios/")) {
+            console.log("Creando carpetas");
+            console.log(sistema);
+            CarpetasWindows();
+        } else {
+          console.log("Las carpetas existen");
+        }
+      break;
+    default:
+      console.log("sin datos");
+      break;
+  }
+};
 
 module.exports = Carpetas;
